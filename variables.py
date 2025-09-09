92dfb267d6c73857682d0c311d3f0c25932e1fac
danielkim321 <dkthor2008@gmail.com>
2025-09-09 13:30:09 -0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -14,3 +14,45 @@ print(x/num)
 
 sum= num + x
 print(sum)
+
+
+
+
+x = 5
+y =10
+z = 0
+z = x + y
+print(z)\
+    
+
+name = "Daniel Kim" 
+occupation = 'student'
+
+print(name)
+
+print(f"my name is {name} and my occupation is {occupation}")
+
+
+first = 'calvert'
+second = 'hall'
+school = first + second 
+print (school)
+
+
+
+
+print=('welcome to calvert hall')
+name = input ('what is your name')
+gradyear = input('what year do you graduate?')
+standing = input('what is your class standing?')
+email = input('what is your email?')
+
+print(f"my name is Daniel, I graduate in {gradyear}, my classs standing is {standing}, and my email is {email}")
+
+
+x = input('what is num1: ')
+y = input ('what is num2:' )
+x= int(x)
+y= int(y) 
+z = x + y
+print (z) 
