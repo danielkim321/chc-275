66880ee65548cae2ef42d117a5706c23de8ea57a
danielkim321 <dkthor2008@gmail.com>
2025-09-11 13:29:17 -0400

    Update branching.py

--- branching.py
+++ branching.py
@@ -1,12 +1,22 @@
-
 x = 10
 
-if x % 2 ==1:
+if x % 2 ==0:
      print(f'{x} is divisible by 2')
      print('test')
 else:
     print('this is not apart of the if statement')
 
+
 if x % 2 ==1:
      print(f'{x} is divisible by 2')
      print('test')
+     
+
+if x % 5 ==0:
+    print(f"{x} is divisible by 5")
+elif x % 2 ==0:
+     print(f'{x} is divisible by 2')
+else: 
+     print(f'{x} is not divisibly by 5 or 2')
+     
+     
