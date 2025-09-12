6f0a69b9129ffeeb03bed10b3fa7bca86376bba5
danielkim321 <dkthor2008@gmail.com>
2025-09-12 14:17:05 -0400

    r34t
    yhyh

--- branching.py
+++ branching.py
@@ -9,7 +9,7 @@ else:
 
 if x % 2 ==1:
      print(f'{x} is divisible by 2')
-     print('test')
+  
      
 
 if x % 5 ==0:
@@ -19,4 +19,25 @@ elif x % 2 ==0:
 else: 
      print(f'{x} is not divisibly by 5 or 2')
      
+
+
+
+x = 10 
+if x > 5 and x > 15:
+     print(f'{x} is greater than 5 and 15')
+if x > 5 or x > 15:
+     print(f'{x} is greater than 5 or 15')
      
+     
+year = input("enter your year:")
+
+if year == 'freshman':
+     print('your grade level is 9')
+elif year == "sophomore":
+     print('your grade level is 10')
+elif year == "junior":
+     print('your grade level is 11')
+elif year == "senior":
+     print('your grade level is 12')
+
+          
