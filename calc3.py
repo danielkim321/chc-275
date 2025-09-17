9a340fd03ab494f3b716143a4d2671be25e81db0
danielkim321 <snyderl27@chcstudent.com>
2025-09-17 09:28:45 -0400

    4

---
+++ calc3.py
@@ -0,0 +1,47 @@
+
+check = False 
+while check  == False:
+
+   
+    
+
+
+    print("welcome to calculator!")
+    print("1. addition")
+    print("2. subtraction")
+    print("3. multipliction")
+    print("4. division")
+
+    option = input("Enter your option addition subtraction division or multiplication. Type quit to exit: ")
+
+    if option == "addition":
+        x = input("add value for x:")
+        x = int(x)
+        y = input('add value for y:')
+        y=int(y)
+        z = x + y
+        print(f'the sum of {x} and {y} is {z}')
+    elif option == "subtraction":
+        r = input("add value for r:")
+        r = int(r)
+        p = input('add value for p:')
+        p =int(p)
+        k = r - p
+        print(f'the difference of {r} and {p} is {k}')
+    elif option == "division":
+        t= input("add value for t:")
+        t = int(t)
+        s = input('add value for s:')
+        s =int(s)
+        g = t / s
+        print(f'the quotient {t} and {s} is {g}')
+    elif option == "multipliction":
+        b = input("add value for b:")
+        b = int(b)
+        c = input('add value for c:')
+        c=int(c)
+        m = b * c
+        print(f'the product of {b} and {c} is {m}')
+    elif option == "quit":
+        check = True
+    
---
+++ looping.py
@@ -0,0 +1,20 @@
+"""""
+x = 1
+while x <= 10:
+    #all numbers from 1 to 10
+    print(x)
+    #x = x + 1 first way 
+    x += 1 #second way 
+"""
+
+check = False 
+while check  == False:
+    option = input("Enter your option 1 2 or 3. Type quit to exit: ")
+    if option == "1":
+        print("option")
+    elif option == "2":
+        print("option 2")
+    elif option == "3":
+        print("option 3")
+    elif option == "quit":
+        check = True 
