930731c62cb6a75829e9fe6c20d8583609ada9f9
danielkim321 <snyderl27@chcstudent.com>
2025-09-18 10:14:22 -0400

    123

---
+++ fish.py
@@ -0,0 +1,19 @@
+
+print("What type of fish do you have?")
+print("1. carnivorous")
+print("2. salt water")
+print("3. community")
+
+option = input("Enter your option carnivorous salt water or community. Type quit to exit: ")
+
+if option == "carnivorous":
+    x = input("do you already have it?:")
+    if x == "yes":
+        print("too bad")
+    if x == "no":
+        print("enjoy!")
+elif option == "salt water":
+         print("fancy fish parent")
+     
+    
+    
