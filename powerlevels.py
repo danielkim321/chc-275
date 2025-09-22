2988b724dcdc8524763d01c5577c61d75c9edaeb
danielkim321 <snyderl27@chcstudent.com>
2025-09-22 09:42:25 -0400

    5

--- powerlevels.py
+++ powerlevels.py
@@ -2,10 +2,11 @@ x  = input("what is the power level of the first monster? ")
 y = input("What is the power level of the second monster")
 x = int(x)
 y = int(y)
-    if x > y:
-     print(f'{x} First monster wins!')
-    if y > x:
-     print(f'{x} Second monster wins!')
-
+if x > y:
+    print(f'{x} First monster wins!')
+elif y > x:
+    print(f'{x} Second monster wins!')
+elif x == y:
+    print("its a tie!")
 
           
