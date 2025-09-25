dd59d78196229b40b4949fff621a8fd6e6104fa3
danielkim321 <snyderl27@chcstudent.com>
2025-09-25 09:29:29 -0400

    897

---
+++ lists.py
@@ -0,0 +1,31 @@
+mylist = [1, 2, 3, 4, 5]
+
+print(mylist[0])
+sum = mylist[0] + mylist [3]
+print(sum)
+
+mylist = [ 5, 10, 15, 20, 25, 30 ,35]
+print('printing size')
+print(len(mylist))
+
+
+i = 0
+while i <= len(mylist)-1:
+    print(mylist[i])
+    i = i + 1
+
+
+
+print("for loop example")
+for x in mylist:
+    x = x + 5
+print(mylist)
+
+i = 0
+while i <= len(mylist)-1:
+    mylist[i] = mylist[i] + 5
+    print(mylist[i])
+    i = i + 1
+    
+print(mylist)
+    
