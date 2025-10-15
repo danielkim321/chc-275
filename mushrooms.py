6425e96c7a8bd7c4195fd09cc36c37a75a1d6026
danielkim321 <dkthor2008@gmail.com>
2025-10-15 10:14:00 -0400

    2

---
+++ mushrooms.py
@@ -0,0 +1,14 @@
+print("what is the size of your mushroom")
+
+
+
+while True:
+
+    option == input('small')
+    def jls_extract_def():
+        
+    
+
+
+    
+    
---
+++ strings2.py
@@ -0,0 +1,12 @@
+space = ""
+
+
+fruits = ["apples", "oranges", "pears", "mango"]
+
+combined = space.join(fruits)
+print(combined)
+
+str1 = "hello"
+str2 = "world"
+concat = str2+str1
+print(concat)
