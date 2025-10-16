85f696d4b0c8f973c437d0366da3e0bee26520cb
danielkim321 <dkthor2008@gmail.com>
2025-10-16 09:29:51 -0400

    2

--- mushrooms.py
+++ mushrooms.py
@@ -1,12 +1,24 @@
-print("what is the size of your mushroom")
 
-
-
-while True:
-
-    option == input('small')
-    def jls_extract_def():
+y= 100 
+w = 200
+p = 300
+x  = input("Enter mushroom size: ")
+x = int(x)
+w = int(w)
+p = int(200)
+y = int(100)
+while True: 
+    if x < y:
+        print(f'{x} small mushroom ')
+    elif x > p:
+        print(f'{x} Big mushroom')
+    elif x < 200 or x > 100:
+        print("Medium mushroom")
+    else:
+        print("Please enter a valid size.") 
         
+   
+
     
 
 
