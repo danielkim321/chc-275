1eaf880e2fc88a55420790e913a06e72e43924b6
danielkim321 <dkthor2008@gmail.com>
2026-03-02 09:25:57 -0500

    0

--- connect4.py
+++ connect4.py
@@ -1,17 +1,92 @@
 def drawBoard(board):
+    for row in board:
+        print(row)
+    print("")
+
+
+def switchPlayer(player):
+    if player == "X":
+        return "O"
+    else:
+        return "X"
+
+
+def dropPiece(board, player, column):
+    for row in range(5, -1, -1):
+        if board[row][column] == 0:
+            board[row][column] = player
+            return True
+    return False
+
+
+def checkWinner(board, player):
+
+    # Horizontal
+    for r in range(6):
+        for c in range(4):
+            if board[r][c] == player:
+                if board[r][c+1] == player:
+                    if board[r][c+2] == player:
+                        if board[r][c+3] == player:
+                            return True
+
+    # Vertical
+    for r in range(3):
+        for c in range(7):
+            if board[r][c] == player:
+                if board[r+1][c] == player:
+                    if board[r+2][c] == player:
+                        if board[r+3][c] == player:
+                            return True
+
+    # Diagonal \
+    for r in range(3):
+        for c in range(4):
+            if board[r][c] == player:
+                if board[r+1][c+1] == player:
+                    if board[r+2][c+2] == player:
+                        if board[r+3][c+3] == player:
+                            return True
+
+    # Diagonal /
+    for r in range(3, 6):
+        for c in range(4):
+            if board[r][c] == player:
+                if board[r-1][c+1] == player:
+                    if board[r-2][c+2] == player:
+                        if board[r-3][c+3] == player:
+                            return True
+
+    return False
 
 
 def main():
-    ROWS = 6
-    COLUMNS = 7
-    BOARD = [ 
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],
-             [0,0,0,0,0,0,0],        
+    board = [
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0]
     ]
-    
-    if __name__ == "__main__":
-    main()
+
+    player = "X"
+
+    while True:
+        drawBoard(board)
+
+        col = int(input("Choose column (0-6): "))
+
+        if dropPiece(board, player, col) == False:
+            print("Column full. Try again.")
+            continue
+
+        if checkWinner(board, player):
+            drawBoard(board)
+            print(player, "wins!")
+            break
+
+        player = switchPlayer(player)
+
+
+main()
